704e37b48782274e1f86f2570128a984e1ff45ad
Ashutosh Singh <ashuforweb@gmail.com>
2015-08-05 00:36:41 -0700

    Test push

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('testButton').addEventListener('click', fnTest, false);
+// TODO: Hello there
+function fnTest () {
+  console.log(document.getElementById('test1').value);
+}
